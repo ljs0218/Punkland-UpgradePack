6b1bc3c0309b2fd004b549b2dacb716dbfb5a879
leejs0218 <58419439+ljs0218@users.noreply.github.com>
2025-03-03 23:03:48 +0900

    feat: 재화 HUD 완성

--- Scripts/UI/HUD/Currency.lua
+++ Scripts/UI/HUD/Currency.lua
@@ -3,20 +3,25 @@ local UI_SETTINGS = LClient.Config.UI.HUD.Currency
 local wrapper = HorizontalPanel()
 wrapper.anchor = Anchor.TopCenter
 wrapper.pivot = Point(0.5, 0)
-wrapper.position = Point(0, 4)
-wrapper.spacing = 4
+wrapper.position = Point(0, 10)
+wrapper.spacing = 6
+
+local currencyControls = {}
 
 for _, currencyId in pairs(UI_SETTINGS.ids) do
     local currencyPanel = HorizontalPanel()
-    currencyPanel.spacing = 4
+    currencyPanel.spacing = 3
     currencyPanel.childAlign = 3
+    
     local currencyIcon = Image(Currency.GetPath(currencyId), Rect(0, 0, 22, 22))
     currencyIcon.anchor = Anchor.MiddleLeft
     currencyIcon.pivot = Point(0, 0.5)
+
     local currencyText = Text("1,000")
     currencyText.textAlign = TextAlign.MiddleLeft
     currencyText.textSize = 14
     currencyText.borderEnabled = true
+    currencyText.borderDistance = Point(0.7, 0.7)
     currencyText.borderColor = Color.black
     currencyText.SetSizeFit(true, true)
 
@@ -25,15 +30,22 @@ for _, currencyId in pairs(UI_SETTINGS.ids) do
     currencyPanel.SetSizeFit(true, true)
     
     wrapper.AddChild(currencyPanel)
+
+    currencyControls[tostring(currencyId)] = {
+        panelControl = currencyPanel,
+        textControl = currencyText
+    }
 end
 
 wrapper.SetSizeFit(true, true)
 
 LClient.Events.onEverySecond:Add(function ()
-    
+    for currencyId, currencyControl in pairs(currencyControls) do
+        currencyControl.textControl.text = LUtility.FormatNumber(Currency.GetAmount(tonumber(currencyId)))
+        currencyControl.textControl.SetSizeFit(true, true)
+        currencyControl.panelControl.SetSizeFit(true, true)
+        currencyControl.panelControl.ForceRebuildLayoutImmediate()
+    end
+    wrapper.SetSizeFit(true, true)
+    wrapper.ForceRebuildLayoutImmediate()
 end)
-
---[[
-gameMoney = unit.gameMoney,
-        varDatas = varDatas,
-]]
